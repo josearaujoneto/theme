cea88d8ea4668d486971bc881a14a3ec4d94d925
Jose <jose.araujo@itriad.org.br>
2025-05-23 23:03:31 -0300

    new palett version

--- src/index.ts
+++ src/index.ts
@@ -7,36 +7,93 @@ import { IThemeManager } from '@jupyterlab/apputils';
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
+import { CatppuccinPalettes } from './palettes';
+
 /**
- * Initialization data for the theme extension.
+ * Initialization data for the catppuccin_jupyterlab extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'theme:plugin',
-  description: 'A JupyterLab extension.',
+  description: '📊 Soothing pastel theme for JupyterLab.',
   autoStart: true,
   requires: [IThemeManager],
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, manager: IThemeManager, settingRegistry: ISettingRegistry | null) => {
-    console.log('JupyterLab extension theme is activated!');
+  activate: (
+    app: JupyterFrontEnd,
+    manager: IThemeManager,
+    settingRegistry: ISettingRegistry | null
+  ) => {
     const style = 'theme/index.css';
+    const palettes = new CatppuccinPalettes();
+
+    let brandColor = 'mauve';
+    let accentColor = 'green';
+
+    function loadSettings(settingRegistry: ISettingRegistry | null) {
+      if (settingRegistry) {
+        settingRegistry
+          .load(plugin.id)
+          .then(settings => {
+            brandColor = settings.get('brandColor').composite as string;
+            accentColor = settings.get('accentColor').composite as string;
+            console.debug(
+              `catppuccin_jupyterlab settings loaded. Brand color is '${brandColor}', Accent color is '${accentColor}'`
+            );
+          })
+          .catch(reason => {
+            console.error(
+              'Failed to load settings for catppuccin_jupyterlab.',
+              reason
+            );
+          });
+      }
+    }
+
+    loadSettings(settingRegistry);
 
     manager.register({
-      name: 'theme',
+      name: 'Catppuccin Latte',
       isLight: true,
-      load: () => manager.loadCSS(style),
+      load: () => {
+        palettes.setColorsLatte();
+        palettes.setConfigColors(brandColor, accentColor);
+        return manager.loadCSS(style);
+      },
       unload: () => Promise.resolve(undefined)
     });
 
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log('theme settings loaded:', settings.composite);
-        })
-        .catch(reason => {
-          console.error('Failed to load settings for theme.', reason);
-        });
-    }
+    manager.register({
+      name: 'Catppuccin Frappé',
+      isLight: false,
+      load: () => {
+        palettes.setColorsFrappe();
+        palettes.setConfigColors(brandColor, accentColor);
+        return manager.loadCSS(style);
+      },
+      unload: () => Promise.resolve(undefined)
+    });
+
+    manager.register({
+      name: 'Catppuccin Macchiato',
+      isLight: false,
+      load: () => {
+        palettes.setColorsMacchiato();
+        palettes.setConfigColors(brandColor, accentColor);
+        return manager.loadCSS(style);
+      },
+      unload: () => Promise.resolve(undefined)
+    });
+
+    manager.register({
+      name: 'Catppuccin Mocha',
+      isLight: false,
+      load: () => {
+        palettes.setColorsMocha();
+        palettes.setConfigColors(brandColor, accentColor);
+        return manager.loadCSS(style);
+      },
+      unload: () => Promise.resolve(undefined)
+    });
   }
 };
 
